17151ea2228ad58eb05f0d6cc2aefcba72b6289f
Sergey Protopopov <Sergey@MacBook-Sergii-Protopopov.local>
2018-10-10 16:33:17 +0300

    Current time&location detection
    Improving for detect current time and location.

--- JS/Moving.js
+++ JS/Moving.js
@@ -8,31 +8,100 @@ var sun = document.getElementById("sun");
 var step = 10;
 
 
+
+//Set Slider with current time...
+function SetSlider(){
+
+	let curentDate = new Date(); // Create object with type Date
+	let h = curentDate.getHours(); // Get current Hours
+	let m = curentDate.getMinutes() // Get current minutes
+
+	slider.value = h; // Set slider to current hour
+
+	output.innerHTML = h.toString() + m.toString(); // Display the default slider value
+	
+}
+
+
+//Detect location for current user
+
+var l = document.getElementById("local");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+
+    } else {
+        l.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+	var crd = position.coords;
+	var lat = crd.latitude;
+    var lon = crd.longitude;
+    var result;
+
+    l.innerHTML = lat + "," + lon;
+   
+    // Sent API request
+	url = "https://api.sunrise-sunset.org/json?lat="+lat+"&lng="+lon+"&formatted=1";
+    result = RequestData(url); 
+
+    console.log(result);
+}
+
+
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            l.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            l.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            l.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            l.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
+
+
 // Request for getting data 
 // form https://sunrise-sunset.org/api
-	function RequestData(){
-		var xhr = new XMLHttpRequest(),
-		    method = "GET",
-	    	url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&formatted=1";
+function RequestData(url){
 
-		xhr.open(method, url, true);
-		xhr.onreadystatechange = function () {
-	        if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-	            Response = JSON.parse(xhr.responseText);
-	            console.log(Response.results);
-	            
-	            return Response.results;
-	        };
+	return new Promise (function (resolve, error) {
 
+		const xhr = new XMLHttpRequest();
+		xhr.open("GET", url, true);
+		
+		xhr.onload = function () {
+	        if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {    
+	            resolve(this.response);
+	        }
+
+	        else{
+	        	var error = new Error(this.statusText);
+	        	error.code = this.status;
+	        	reject(error);
+	        }
 	    };
+
+	    xhr.onerror = function(){
+	    	reject(new Error("Network Error"));
+	    };
+
 		xhr.send();
-	}
+	    });
+}
 
 
-RequestData(); // look at console
-
-
-output.innerHTML = slider.value; // Display the default slider value
+SetSlider();
 
 
 // Update the current slider value (each time you drag the slider handle)
